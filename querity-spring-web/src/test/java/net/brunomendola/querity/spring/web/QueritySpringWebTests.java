898a20a1a9e0bb46032d553f9b403d3b4307ae51
Bruno Mendola <1853562+brunomendola@users.noreply.github.com>
2021-11-15 19:38:13 +0100

    Refactored multiple similar tests to a single parameterized test

--- querity-spring-web/src/test/java/net/brunomendola/querity/spring/web/QueritySpringWebTests.java
+++ querity-spring-web/src/test/java/net/brunomendola/querity/spring/web/QueritySpringWebTests.java
@@ -1,6 +1,7 @@
 package net.brunomendola.querity.spring.web;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
@@ -10,36 +11,22 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-/**
- * Tests JSON deserialization and serialization of a Query object given as a REST endpoint query parameter
- */
 @WebMvcTest(controllers = QueritySpringWebMvcTestController.class)
 @Import(QueritySpringWebAutoConfiguration.class)
 class QueritySpringWebTests {
   @Autowired
   MockMvc mockMvc;
 
-  @Test
-  void givenJsonQueryWithSimpleConditionAsQueryParameter_whenGetQuery_thenReturnTheSameQueryAsResponse() throws Exception {
-    String query = "{\"filter\":{\"propertyName\":\"lastName\",\"operator\":\"EQUALS\",\"value\":\"Skywalker\"}}";
-    mockMvc.perform(get("/query")
-            .queryParam("q", query))
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(content().json(query, false));
-  }
-
-  @Test
-  void givenJsonQueryWithConditionsWrapperAsQueryParameter_whenGetQuery_thenReturnTheSameQueryAsResponse() throws Exception {
-    String query = "{\"filter\":{\"logic\":\"AND\",\"conditions\":[{\"propertyName\":\"lastName\",\"operator\":\"EQUALS\",\"value\":\"Skywalker\"}]}}";
-    mockMvc.perform(get("/query")
-            .queryParam("q", query))
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(content().json(query, false));
-  }
-
-  @Test
-  void givenJsonQueryWithNestedConditionsWrapperAsQueryParameter_whenGetQuery_thenReturnTheSameQueryAsResponse() throws Exception {
-    String query = "{\"filter\":{\"logic\":\"AND\",\"conditions\":[{\"propertyName\":\"lastName\",\"operator\":\"EQUALS\",\"value\":\"Skywalker\"},{\"logic\":\"OR\",\"conditions\":[{\"propertyName\":\"firstName\",\"operator\":\"EQUALS\",\"value\":\"Anakin\"},{\"propertyName\":\"firstName\",\"operator\":\"EQUALS\",\"value\":\"Luke\"}]}]}}";
+  /**
+   * Tests JSON deserialization and serialization of a Query object given as a REST endpoint query parameter
+   */
+  @ParameterizedTest
+  @ValueSource(strings = {
+      /* single simple condition */   "{\"filter\":{\"propertyName\":\"lastName\",\"operator\":\"EQUALS\",\"value\":\"Skywalker\"}}",
+      /* conditions wrapper */        "{\"filter\":{\"logic\":\"AND\",\"conditions\":[{\"propertyName\":\"lastName\",\"operator\":\"EQUALS\",\"value\":\"Skywalker\"}]}}",
+      /* nested conditions wrapper */ "{\"filter\":{\"logic\":\"AND\",\"conditions\":[{\"propertyName\":\"lastName\",\"operator\":\"EQUALS\",\"value\":\"Skywalker\"},{\"logic\":\"OR\",\"conditions\":[{\"propertyName\":\"firstName\",\"operator\":\"EQUALS\",\"value\":\"Anakin\"},{\"propertyName\":\"firstName\",\"operator\":\"EQUALS\",\"value\":\"Luke\"}]}]}}"
+  })
+  void givenJsonQuery_whenGetQuery_thenReturnTheSameQueryAsResponse(String query) throws Exception {
     mockMvc.perform(get("/query")
             .queryParam("q", query))
         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
