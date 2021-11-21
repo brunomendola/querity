79cdedc7ab0eb399bf75a8c2e98b22a9cf1dd0a0
Bruno Mendola <1853562+brunomendola@users.noreply.github.com>
2021-11-21 02:50:28 +0100

    Added missing tests

---
+++ querity-api/src/test/java/net/brunomendola/querity/api/QueryTests.java
@@ -0,0 +1,51 @@
+package net.brunomendola.querity.api;
+
+import org.junit.jupiter.api.Test;
+
+import static net.brunomendola.querity.api.Operator.EQUALS;
+import static net.brunomendola.querity.api.Querity.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class QueryTests {
+  @Test
+  void givenNoFilter_whenHasFilter_thenReturnsFalse() {
+    Query query = Querity.query().build();
+    assertThat(query.hasFilter()).isFalse();
+  }
+
+  @Test
+  void givenFilter_whenHasFilter_thenReturnsTrue() {
+    Query query = Querity.query().filter(filterBy("lastName", EQUALS, "Skywalker")).build();
+    assertThat(query.hasFilter()).isTrue();
+  }
+
+  @Test
+  void givenNoPagination_whenHasPagination_thenReturnsFalse() {
+    Query query = Querity.query().build();
+    assertThat(query.hasPagination()).isFalse();
+  }
+
+  @Test
+  void givenPagination_whenHasPagination_thenReturnsTrue() {
+    Query query = Querity.query().pagination(1, 20).build();
+    assertThat(query.hasPagination()).isTrue();
+  }
+
+  @Test
+  void givenPagination2_whenHasPagination_thenReturnsTrue() {
+    Query query = Querity.query().pagination(paged(1, 20)).build();
+    assertThat(query.hasPagination()).isTrue();
+  }
+
+  @Test
+  void givenNoSort_whenHasSort_thenReturnsFalse() {
+    Query query = Querity.query().build();
+    assertThat(query.hasSort()).isFalse();
+  }
+
+  @Test
+  void givenSort_whenHasSort_thenReturnsTrue() {
+    Query query = Querity.query().sort(sortBy("lastName")).build();
+    assertThat(query.hasSort()).isTrue();
+  }
+}
--- querity-common/src/test/java/net/brunomendola/querity/common/util/PropertyUtilsTests.java
+++ querity-common/src/test/java/net/brunomendola/querity/common/util/PropertyUtilsTests.java
@@ -1,5 +1,6 @@
 package net.brunomendola.querity.common.util;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -8,6 +9,7 @@ import java.math.BigDecimal;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PropertyUtilsTests {
 
@@ -26,6 +28,38 @@ class PropertyUtilsTests {
   @MethodSource("provideBeanPropertiesAndTypes")
   void givenBeanProperty_whenGetPropertyType_thenReturnCorrectType(Class<?> beanClass, String propertyPath, Class<?> expectedPropertyType) {
     assertThat(PropertyUtils.getPropertyType(beanClass, propertyPath)).isEqualTo(expectedPropertyType);
+  }
+
+  @Test
+  void givenNoxExistingBeanProperty_whenGetPropertyType_thenThrowsIllegalArgumentException() {
+    assertThrows(IllegalArgumentException.class,
+        () -> PropertyUtils.getPropertyType(MyClass.class, "nonExisting"),
+        "Property nonExisting not found in class MyClass");
+  }
+
+  @Test
+  void givenNull_whenGetActualPropertyValue_thenReturnNull() {
+    assertThat(PropertyUtils.getActualPropertyValue(MyClass.class, "stringValue", null)).isNull();
+  }
+
+  public static Stream<Arguments> provideValuesAndTypes() {
+    return Stream.of(
+        Arguments.of(MyClass.class, "stringValue", "test", String.class),
+        Arguments.of(MyClass.class, "intValue", "1", Long.class),
+        Arguments.of(MyClass.class, "intValue", 1, Integer.class),
+        Arguments.of(MyClass.class, "integerValue", "1", Long.class),
+        Arguments.of(MyClass.class, "integerValue", 1, Integer.class),
+        Arguments.of(MyClass.class, "bigDecimalValue", "42.00", BigDecimal.class),
+        Arguments.of(MyClass.class, "doubleValue", "1.2", BigDecimal.class),
+        Arguments.of(MyClass.class, "doubleValue", 1.2, Double.class),
+        Arguments.of(MyClass.class, "nested.stringValue", "test", String.class)
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideValuesAndTypes")
+  void givenValue_whenGetActualPropertyValue_thenReturnValueWithCorrectType(Class<?> beanClass, String propertyPath, Object value, Class<?> expectedType) {
+    assertThat(PropertyUtils.getActualPropertyValue(beanClass, propertyPath, value).getClass()).isEqualTo(expectedType);
   }
 
   public static class MyClass {
